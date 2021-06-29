ab89fed00fb0f18d66feb4b042e60664a68dbaeb
Talhimohammed <talhimohammed507@gmail.com>
2021-06-29 15:39:16 +0100

    changes

---
+++ src/Components/Data/Curriencies.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+
+export function getCurriencies(){
+    return fetch("https://api.swapspace.co/api/currencies",
+    {
+      method: 'GET', 
+      mode: 'cors',
+      headers: { 
+        'Accept': 'application/json',
+        "Authorization": 'a3b5214009a3a808a19819c917ce61de1f775b1c723058bbdaed3ffa5e3e79bc57c5c61b9eea3c1887'
+       
+      }
+    })
+      .then(data => data.json());
+
+  }
+  export function Convert(code,usd){ 
+    return fetch("https://api.swapspace.co/api/amounts?from=btc&to="+code+"&amount=0.1",
+    {
+      method: 'GET', 
+      mode: 'cors',
+      headers: { 
+        'Accept': 'application/json',
+        "Authorization": 'a3b5214009a3a808a19819c917ce61de1f775b1c723058bbdaed3ffa5e3e79bc57c5c61b9eea3c1887'
+       
+      }
+    })
+      .then(data => data.json());
+
+  } 
+
+   
+  
+
+
